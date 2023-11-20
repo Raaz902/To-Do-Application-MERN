fd10597ef00dd5051fe3bd7cd8e5c76405569252
Mohammad Raaz <mohdraaz902@gmail.com>
2023-11-20 16:36:47 +0530

    final commit

--- Frontend/src/components/ToDo.js
+++ Frontend/src/components/ToDo.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 export default function ToDo() {
     const [task, setTask] = useState('');
-    const [tasks, setTasks] = useState();
-    console.log(tasks)
+    const [tasks, setTasks] = useState(null);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const result = await getProducts();
-                console.log(result)
-
+                console.log(result);
             } catch (error) {
                 console.error("Error fetching products:", error);
             }
@@ -17,53 +16,78 @@ export default function ToDo() {
 
         fetchData();
     }, []);
+
     const getProducts = async () => {
         let result = await fetch('http://localhost:8000/tasks');
         result = await result.json();
         setTasks(result);
         return result;
-    }
+    };
+
     const addTask = async () => {
         if (!task) {
-            alert("Enter Task")
+            alert("Enter Task");
         }
-        const userId = JSON.parse(localStorage.getItem('user'))._id
-        //console.log(userId)
+        const userId = JSON.parse(localStorage.getItem('user'))._id;
         let result = await fetch("http://localhost:8000/addtask", {
             method: "post",
             body: JSON.stringify({ task, userId, status: false }),
             headers: {
-                "Content-Type": "application/json"
-            }
+                "Content-Type": "application/json",
+            },
         });
         result = await result.json();
-        console.log(result)
+        console.log(result);
         setTask('');
         getProducts();
+    };
 
-    }
     const handleCheckboxChange = async (data) => {
-        console.log(data);
-
-        // const userId = JSON.parse(localStorage.getItem('user'))._id;
-        //console.log(userId)
         let result = await fetch(`http://localhost:8000/update/${data._id}`, {
             method: "put",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({ task: data.task, userId: data.userId, status: !data.status })
+            body: JSON.stringify({ task: data.task, userId: data.userId, status: !data.status }),
         });
         result = await result.json();
         getProducts();
-    }
+    };
+
     const deleteTask = async (data) => {
         const result = await fetch(`http://localhost:8000/delete/${data}`, {
-            method: "DELETE"
+            method: "DELETE",
         });
         getProducts();
         return result.ok;
-    }
+    };
+
+    const handleDragStart = (e, index) => {
+
+        e.dataTransfer.setData('text/plain', index.toString());
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e, targetIndex) => {
+        e.preventDefault();
+
+        const draggedIndex = parseInt(e.dataTransfer.getData('text/plain'), 10);
+
+
+        if (draggedIndex === targetIndex) {
+            return;
+        }
+
+        const updatedTasks = [...tasks];
+        const [draggedTask] = updatedTasks.splice(draggedIndex, 1);
+        updatedTasks.splice(targetIndex, 0, draggedTask);
+
+
+        setTasks(updatedTasks);
+    };
 
     return (
         <div className='todoDiv'>
@@ -79,22 +103,28 @@ export default function ToDo() {
                         <th className='status-header'>Delete Task</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody onDragOver={handleDragOver}>
                     {tasks ?
                         tasks.map((item, index) => (
-                            <tr key={index + 1} className={item.status ? 'completed-row' : 'pending-row'}>
+                            <tr key={index + 1}
+                                className={item.status ? 'completed-row' : 'pending-row'}
+                                onDragStart={(e) => handleDragStart(e, index)}
+                                onDragOver={(e) => handleDragOver(e)}
+                                onDrop={(e) => handleDrop(e, index)}
+                                draggable="true"
+                            >
                                 <td className='checkbox-cell'>
                                     <input type="checkbox" className='checkbox' checked={item.status} onChange={() => handleCheckboxChange(item)} />
                                 </td>
                                 <td className='task-cell'>{item.task}</td>
                                 <td className='status-cell'>{item.status === false ? "Pending" : 'Done ✅'}</td>
-                                <td className='status-cell'><button onClick={(e)=>deleteTask(item._id)}>Delete</button></td>
+                                <td className='status-cell'><button onClick={() => deleteTask(item._id)}>Delete</button></td>
                             </tr>
                         ))
                         : null}
                 </tbody>
             </table>
         </div>
-
-    )
+    );
 }
+
